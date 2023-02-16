6606800c9cdd81a43f451e5550d6d403b6099f70
nSystemz <78032251+nSystemz@users.noreply.github.com>
2023-02-16 15:10:06 +0100

    Anpassungen auf neuste ESX Version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,12 +4,14 @@ games {'gta5'}
 author 'Nemesus.de'
 description 'Wir sind die coolsten GTA5 Edition'
 contact 'info@nemesus.de'
-version '1.0'
+version '1.1'
 
 dependencies {
 	"es_extended",
 	"xsound"
 }
+
+shared_script '@es_extended/imports.lua'
 
 client_script 'client.lua'
 
